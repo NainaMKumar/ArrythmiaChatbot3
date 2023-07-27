7e1bcb1473a321c9b8c102c508e39621f6c350a8
NainaMKumar <nainamkumar@gmail.com>
2023-07-27 19:52:13 -0400

    Initial commit

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,8 @@ import { Configuration, OpenAIApi } from "openai";
 import express from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
+import { TextToSpeechClient } from "@google-cloud/text-to-speech"; // Import the Google Text-to-Speech client
+import { franc } from "franc"; // Use named import for franc
 
 const app = express();
 const port = 8000;
@@ -10,40 +12,126 @@ app.use(cors());
 
 require('dotenv').config();
 
-console.log("Before chat completion");
 const configuration = new Configuration({
   organization: process.env.ORG_ID,
   apiKey: process.env.API_KEY,
 });
 
-console.log("After chat completion")
 const openai = new OpenAIApi(configuration);
 
-app.post("/", async (request, response) => {
-  const { chats } = request.body;
-
-  const result = await openai.createChatCompletion({
-    model: "gpt-3.5-turbo",
-    messages: [
-      {
-        role: "system",
-        content: "You are a sympathetic doctor that helps patients with arrythmia",
-      },
-      ...chats,
-    ],
-  });
-
-  const output = result.data.choices[0].message;
-  console.log(output);
-
-  response.json({
-    output: output,
-  });
+// Initialize Google Text-to-Speech client
+const ttsClient = new TextToSpeechClient({
+  projectId: "proven-aura-382621",
+  keyFilename: "C:/Users/lalikumar/Downloads/proven-aura-382621-e2e57a828f89.json",
 });
 
+app.post("/", async (request, response) => {
+  const {chats, formData} = request.body
+
+  if (formData !== undefined) {
+    try {
+      // Handle the audio file sent by the user
+      const userAudio = formData.get("userAudio")
+  
+      // Perform language detection on the chatbot's response
+      let detectedLanguage;
+      try {
+        detectedLanguage = franc(userAudio.toString());
+      } catch (error) {
+        console.error("Error detecting language:", error);
+        detectedLanguage = "en"; // Default to English if language detection fails
+      }
+  
+      const result = await openai.createChatCompletion({
+        model: "gpt-3.5-turbo",
+        messages: [
+          {
+            role: "system",
+            content: "You are an expert in heart arrythmias",
+          },
+          ...userAudio,
+        ],
+      });
+  
+      const output = result.data.choices[0].message;
+      console.log(output);
+  
+      // Define a dictionary to map the detected language to language code and voice name
+      const language_dict = {
+        en: { language_code: "en-US", voice_name: "en-US-Wavenet-D" },
+        hi: { language_code: "hi-IN", voice_name: "hi-IN-Wavenet-A" },
+      };
+  
+      // Set the language and voice for Google TTS based on the detected language
+      const languageCode = language_dict[detectedLanguage]
+        ? language_dict[detectedLanguage].language_code
+        : "en-US";
+      const voiceName = language_dict[detectedLanguage]
+        ? language_dict[detectedLanguage].voice_name
+        : "en-US-Wavenet-D";
+  
+      // Convert the chatbot's response to speech using Google TTS API
+      try {
+        const [speechResponse] = await ttsClient.synthesizeSpeech({
+          input: { text: output },
+          voice: {
+            languageCode: languageCode, // Use detected language or default to "en-US"
+            name: voiceName, // Use detected voice name or default to "en-US-Wavenet-D"
+          },
+          audioConfig: { audioEncoding: "MP3" }, // Adjust audio encoding as needed (e.g., "MP3", "LINEAR16", etc.)
+        });
+  
+        const audioData = speechResponse.audioContent;
+  
+        // Set the appropriate response headers
+        response.setHeader("Content-Type", "audio/mpeg");
+        response.setHeader("Content-Disposition", 'attachment; filename="speech.mp3"');
+  
+        // Send the audio data back to the client
+        response.send(audioData);
+  
+      } catch (err) {
+        console.error("Error converting text to speech:", err);
+        response.status(500).json({ error: "Error converting text to speech" });
+      }
+  
+    } catch (error) {
+      console.error("Error processing audio:", error);
+      response.status(500).json({ error: "Error processing audio" });
+    }
+  }
+
+  else if (chats !== undefined) {
+    const result = await openai.createChatCompletion({
+      model: "gpt-3.5-turbo",
+      messages: [
+        {
+          role: "system",
+          content: "You are a sympathetic doctor that helps patients with arrythmia",
+        },
+        ...chats,
+      ],
+    });
+  
+    const output = result.data.choices[0].message;
+    console.log(output);
+  
+    response.json({
+      output: output
+    })
+  }
+
+  else {
+    response.status(400).json({ error: "Invalid request. Please provide either 'formData' or 'chats' in the request body." });
+  }
+
+});
+  
+  
 app.listen(port, () => {
   console.log(`listening on port ${port}`);
 });
+
 
 
 
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,43 +1,107 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 import "./App.css";
 
 function App() {
   const [message, setMessage] = useState("");
   const [chats, setChats] = useState([]);
   const [isTyping, setIsTyping] = useState(false);
+  const mediaRecorderRef = useRef(null); // Reference to the MediaRecorder instance
+  const [recordedAudioBlob, setRecordedAudioBlob] = useState(null); // State to store the recorded audio data
+  const [audioMessages, setAudioMessages] = useState([]);
 
-  const chat = async (e, message) => {
-    e.preventDefault();
 
-    if (!message) return;
-    setIsTyping(true);
-    scrollTo(0, 1e10);
+  const chat = async () => {
+    if (recordedAudioBlob) {
 
-    let msgs = chats;
-    msgs.push({ role: "user", content: message });
-    setChats(msgs);
+      setIsTyping(true);
+      const formData = new FormData();
+      formData.append("userAudio", recordedAudioBlob);
 
-    setMessage("");
+      try {
+        const response = await fetch("http://localhost:8000/", {
+          method: "POST",
+          body: formData,
+        });
 
-    fetch("http://localhost:8000/", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        chats,
-      }),
-    })
-      .then((response) => response.json())
-      .then((data) => {
+        if (response.ok) {
+          const data = await response.blob();
+          const audio = new Audio(URL.createObjectURL(data));
+
+          setAudioMessages((prevAudioMessages) => [...prevAudioMessages, { role: "api_audio", content: audio}]);
+          setIsTyping(false);
+          scrollTo(0, 1e10);
+
+        } else {
+          console.log("No audio response");
+        }
+      } catch (error) {
+        console.error("Error processing audio:", error);
+      }
+    }
+
+    else {
+      setIsTyping(true);
+      scrollTo(0, 1e10);
+
+      let msgs = chats;
+      msgs.push({ role: "user", content: message });
+      setChats(msgs);
+
+      setMessage("");
+
+      try {
+        const response = await fetch("http://localhost:8000/", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            chats,
+          }),
+        });
+        const data = await response.json();
+
+        msgs = chats;
         msgs.push(data.output);
         setChats(msgs);
+
         setIsTyping(false);
         scrollTo(0, 1e10);
-      })
-      .catch((error) => {
-        console.log(error);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    }
+  };
+
+  const startRecording = () => {
+    try {
+      navigator.mediaDevices.getUserMedia({ audio: true }).then((stream) => {
+        const mediaRecorder = new MediaRecorder(stream);
+        mediaRecorderRef.current = mediaRecorder;
+
+        const audioChunks = [];
+        mediaRecorder.ondataavailable = (event) => {
+          audioChunks.push(event.data);
+        };
+
+        mediaRecorder.onstop = () => {
+          const recordedAudioBlob = new Blob(audioChunks, { type: "audio/webm" });
+          setRecordedAudioBlob(recordedAudioBlob); // Store the recorded audio data
+          setAudioMessages((prevAudioMessages) => [...prevAudioMessages, { role: "user_audio", content: recordedAudioBlob}]);
+          chat();
+        };
+
+        mediaRecorder.start()
       });
+    } catch (error) {
+      console.error("Error starting recording:", error);
+    }
+  };
+
+  const stopRecording = () => {
+    if (mediaRecorderRef.current) {
+      mediaRecorderRef.current.stop();
+    }
   };
 
   return (
@@ -45,24 +109,51 @@ function App() {
       <h1> Arrhythmia Chatbot</h1>
 
       <section>
-        {chats && chats.length
-          ? chats.map((chat, index) => (
-              <p
-                key={index}
-                className={`message ${chat.role === "user" ? "user_msg" : "api_msg"}`}
-              >
-                <span>{chat.content}</span>
-              </p>
-            ))
+        {audioMessages && audioMessages.length > 0
+          ? audioMessages.map((audio, index) => {
+            if (audio.role === "user_audio") {
+              return (
+                <div key={index} className="user_audio">
+                  <audio controls src={URL.createObjectURL(audio.content)} />
+                </div>
+              );
+            } else if (audio.role === "api_audio") {
+              return (
+                <div key={index} className="api_audio">
+                  <audio controls src={URL.createObjectURL(audio.content)} />
+                </div>
+              );
+            }
+          })
           : ""}
       </section>
-  
+
+      <section>
+        {chats && chats.length > 0
+          ? chats.map((chat, index) => {
+            if (chat.role === "user") {
+              return (
+                <p key={index} className="user_msg">
+                  <span>{chat.content}</span>
+                </p>
+              );
+            } else if (chat.role === "api") {
+              return (
+                <p key={index} className="api_msg">
+                  <span>{chat.content}</span>
+                </p>
+              );
+            }
+          })
+          : ""}
+      </section>
+
       <div className={isTyping ? "" : "hide"}>
         <p>
           <i>{isTyping ? "Typing" : ""}</i>
         </p>
       </div>
-  
+
       <form onSubmit={(e) => chat(e, message)}>
         <input
           type="text"
@@ -73,9 +164,13 @@ function App() {
         />
         <button type="submit"> ➤ </button>
       </form>
+
+      <div className="recording">
+        <button id="start-button" onClick={startRecording}>⏺</button>
+        <button id="stop-button" onClick={stopRecording}>⏹</button>
+      </div>
     </main>
   );
-  
 }
 
 export default App;
